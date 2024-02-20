a218891e4947653edd1b0f4bff10ec4744434296
scott.giles.pro <scott.giles.pro@gmail.com>
2024-02-20 16:14:47 -0500

    20240220 commit

--- reactplayground/src/MyBio.js
+++ reactplayground/src/MyBio.js
@@ -1,4 +1,4 @@
-
+import React, { useEffect, useState } from "react";
 
 
 function MyBio() {
@@ -10,6 +10,12 @@ function MyBio() {
     console.log("month_diff: " + month_diff);
     console.log("age_dt: " + age_dt);
     console.log("age: " + age);
+
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleOnChange = () => {
+        setIsChecked(!isChecked);
+    };
     
     return (
         <div>
@@ -17,10 +23,16 @@ function MyBio() {
             <ul>
                 <li>Blonde har (shaved)</li>
                 <li>Blue eyes</li>
-                <li>Athletic</li>
                 <li>Loves to code</li>
                 <li>JS computes that I am {age} years old!</li>
             </ul>
+            <h2>My Favorite Pizza Toppings Are</h2>
+            <div className="topping">
+                <input type="checkbox" id="topping" value="Paneer" checked={isChecked} onChange={handleOnChange} />
+            </div>
+            <div className="result">
+                Above checkbox is {isChecked ? "checked" : "un-checked"}.
+            </div>
         </div>
     )
 }
