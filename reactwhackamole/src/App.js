ffdaffe931b710706b8997e30a1018389020fb4a
scott.giles.pro <scott.giles.pro@gmail.com>
2024-08-07 15:58:33 -0400

    Flight Widget in progress

--- reactwhackamole/src/App.js
+++ reactwhackamole/src/App.js
@@ -5,23 +5,55 @@ import hole from './assets/hole.png';
 import mole from './assets/mole.png';
 
 function App() {
-  const [moles, setMoles] = useState(new Array(9).fill(true));
+  const [score, setScore] = useState(0);
+  const [moles, setMoles] = useState(new Array(9).fill(false));
+
+  function whackMole(index: number) {
+    if (!moles[index]) return;
+    hideMole(index);
+    setScore(score + 1);
+  }
+
+  function hideMole(index: number) {
+    if (!moles[index]) return;
+    const newMoles = [...moles];
+    newMoles[index] = false;
+    setMoles(newMoles);
+  }
+
+  function popMole(index: number) {
+    const newMoles = [...moles];
+    newMoles[index] = true;
+    setMoles(newMoles);
+  }
 
   useEffect(() => {
     const interval = setInterval(() => {
-
+      const randomIndex = Math.floor(Math.random() * moles.length);
+      popMole(randomIndex);
+      setTimeout(() => {
+        hideMole(randomIndex);
+      }, 500)
     }, 1000)
 
     return () => {
       clearInterval(interval);
     }
-  }, [])
+  }, [moles])
+
   return (
+    <>
+    <h1>Score {score}</h1>
     <div className="grid">
-      {moles.map((isMole) => (
-        <img src={isMole ? mole : hole} />
+      {moles.map((isMole, idx) => (
+        <img src={isMole ? mole : hole} 
+        onClick={() => {whackMole(idx);
+          
+        }}
+        />
       ))}
     </div>
+    </>
   );
 }
 
